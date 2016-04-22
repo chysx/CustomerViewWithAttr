f8aa46e1ae53219cd48dc13cc5985522e72f5bec
chysx <976990321@qq.com>
2016-04-22 10:02:05 +0800

    1.修改文件

--- app/src/main/java/zhang/com/customerviewwithattr/CustomerView.java
+++ app/src/main/java/zhang/com/customerviewwithattr/CustomerView.java
@@ -1,7 +1,18 @@
 package zhang.com.customerviewwithattr;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by beiyong14 on 2016/4/21.
  */
-public class CustomerView {
+public class CustomerView extends View{
+    public CustomerView(Context context) {
+        super(context);
+    }
+
+    public CustomerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
